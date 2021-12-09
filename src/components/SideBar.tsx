5da9c357657098dca04ce4ce7c49633c95db1be8
Luis Gustavo <luisgssf@gmail.com>
2021-12-09 18:32:21 -0300

    Componentes Sidebar e Content criados e funcionais

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: Array<Genre>;
+  selectedGenreId: number;
+  selectGenreId: (id: number) => void;
+}
+
+export function SideBar({ genres, selectGenreId, selectedGenreId}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => selectGenreId(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
